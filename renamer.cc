814d419f04bbb695f1f2d36680d0e63931821577
Sirajus Salekin <ssalek14@earlham.edu>
2023-02-08 21:48:37 -0500

    Implemented small straightforward functions

--- renamer.cc
+++ renamer.cc
@@ -1,4 +1,5 @@
 #include "renamer.h"
+#include <stdexcept>
 
 renamer::renamer(uint64_t n_log_regs,
         uint64_t n_phys_regs,
@@ -19,13 +20,22 @@ renamer::renamer(uint64_t n_log_regs,
     al.tail = 0;
     al.head_phase = 0;
     al.tail_phase = 0;    
+    int i;
     //TODO: initiate all fields to 0 in the active list entry
+    for (i=0; i<n_active; i++){
+        al.list[i] = {};
+    }
+    
 }
 
-void renamer::commit(){
-    return;
+bool renamer::is_ready(uint64_t phys_reg){
+    //TODO: check validity of the input
+    //TODO: is this phys_reg 0 or 1 indexed?
+    return prf_ready[phys_reg];
 }
+
 bool renamer::stall_branch(uint64_t bundle_branch){
+    //TODO: IS THIS THE RIGHT ALGORITHM?
     //if the number of zero's in GBM is less than bundle_branch return true
     //return false otherwise TODO: is there multiple value of GBM
     int one_bit_counter;
@@ -44,3 +54,39 @@ bool renamer::stall_branch(uint64_t bundle_branch){
 
     return true;
 } 
+
+void renamer::commit(){
+    
+    return;
+}
+
+void renamer::squash(){
+    //TODO: Not Implemented
+    //What does squashing entail?
+    //zero out all the instructions in AL? or put the HEAD and TAIL together?
+    //copy AMT to RMT
+    //What else is involved in squashing a renamer with AMT+RMT?
+    
+    return;
+}
+
+void renamer::set_exception(uint64_t AL_index){
+    this->al.list[AL_index].exception = 1;
+}
+
+void renamer::set_load_violation(uint64_t AL_index){
+    this->al.list[AL_index].load_violation = 1;
+}
+
+void renamer::set_branch_misprediction(uint64_t AL_index){
+    this->al.list[AL_index].br_mispredict = 1;
+}
+
+void renamer::set_value_misprediction(uint64_t AL_index){
+    this->al.list[AL_index].val_mispredict = 1;
+}
+
+bool renamer::get_exception(uint64_t AL_index){
+    //TODO: throw exception if AL_index is invalid
+    return this->al.list[AL_index].exception; 
+}
